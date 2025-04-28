a1110ec5b729c40608467b7cfea79606ea774138
Erremme <Erremme2.0@gmail.com>
2025-04-28 18:36:22 +0200

    feat: snack-3

---
+++ Snack-3/main.js
@@ -0,0 +1,12 @@
+/*
+Crea una funzione eseguiOperazione
+Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri.
+ */
+
+const add = ( num1 , num2) => num1 + num2;
+
+const eseguiOperazione = (num1, num2, callBack) => {
+console.log(callBack(num1, num2));}
+
+eseguiOperazione(5, 10, add);
+ 
